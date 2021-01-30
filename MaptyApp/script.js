58974ecb7a5340850e021d61706f77fb9a8ad0b7
Bozhidar Nemski <bojonemski@gmail.com>
2021-01-30 17:12:23 +0200

    Fixed rounding speed field

--- MaptyApp/script.js
+++ MaptyApp/script.js
@@ -203,7 +203,7 @@ class App {
             html += `
              <div class="workout__details">
             <span class="workout__icon">⚡️</span>
-            <span class="workout__value">${workout.speed}</span>
+            <span class="workout__value">${workout.speed.toFixed(1)}</span>
             <span class="workout__unit">km/h</span>
           </div>
           <div class="workout__details">
